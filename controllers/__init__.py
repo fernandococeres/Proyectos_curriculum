26f6fe8aacb6b4af8e3701052326d99ac9115a32
fernandococeres <fernandococeres22@gmail.com>
2026-05-12 13:57:15 -0300

    Delete controllers directory

--- controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Módulo de controladores."""
-from .controlers import ControladorAlumnos, ControladorProfesores
-from .admin import AdminAlumnos, AdminProfesores
-
-__all__ = ['ControladorAlumnos', 'ControladorProfesores', 'AdminAlumnos', 'AdminProfesores']
